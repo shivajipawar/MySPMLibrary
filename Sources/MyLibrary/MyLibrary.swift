5d135b70998338c6e2f35735ecf039bad95c754e
shivajinpawar <shivajinpawar@gmail.com>
2025-09-07 17:18:11 +0530

    Add Dependency Key

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,5 +1,11 @@
 public struct MyLibrary {
     public private(set) var text = "Hello, World!"
+    public var key:String{
+        get{
+            "93d33998099812f88a57c27a3fa05551"
+        }
+    }
+    
 
     public init() {
     }
