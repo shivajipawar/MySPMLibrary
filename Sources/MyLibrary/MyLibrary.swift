a16758d8d1027e52c90a15bf7cae3d29a5dbfa50
shivajinpawar <shivajinpawar@gmail.com>
2025-09-07 18:21:58 +0530

    Add API Key shared configuration

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,12 +1,9 @@
-public struct MyLibrary {
-    public private(set) var text = "Hello, World!"
-    public var key:String{
+public final class Configuration{
+    private init(){}
+    static let shared = Configuration()
+    public var ApiKey:String{
         get{
             "93d33998099812f88a57c27a3fa05551"
         }
-    }
-    
-
-    public init() {
     }
 }
