e6fd803dbdfaaed6cc35c64eb1237c6dc9819bd3
shivajinpawar <shivajinpawar@gmail.com>
2025-09-07 18:25:41 +0530

    Make shared configuration public

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,6 +1,6 @@
 public final class Configuration{
     private init(){}
-    static let shared = Configuration()
+    public static let shared = Configuration()
     public var ApiKey:String{
         get{
             "93d33998099812f88a57c27a3fa05551"
